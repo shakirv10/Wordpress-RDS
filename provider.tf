3340f2fd08764a0f9105edc3a2cef5a746ca55b3
Shakir Villanueva <75804008+shakirv10@users.noreply.github.com>
2022-05-05 17:21:36 +0200

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+# provider.tf
+ 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.26"
+    }
+  }
+}
+ 
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
